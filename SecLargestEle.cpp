967db2091151be7761932d5649db7783f4566fa5
Atlanta Gogoi <atlantagogoi516@gmail.com>
2024-07-20 14:10:01 +0530

    Problem 23 solved in: 20/07/2024

---
+++ SecLargestEle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+int solve(int n, std::vector<int> arr){
+    sort(arr.begin(), arr.end());
+    int min_ele = -1;
+    for(int i=n-2; i>=0; i--)
+    {
+        if(arr[i]<arr[n-1])
+        {    
+            min_ele = arr[i];
+            break;
+        }
+    }
+    return min_ele;
+}
+
+int main()
+{
+    int s, res;
+    std::cin>>s;
+    std::vector<int> num (s);
+    for(int i=0; i<s; i++)
+        std::cin>>num[i];
+    res = solve(s, num);
+    std::cout<<res;
+    return 0;
+}
